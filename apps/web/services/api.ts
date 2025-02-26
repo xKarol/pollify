14f8d4ae2d98d53cb53d00be5f96b6fe86250cf7
Karol Piskorz <karolpiskorz8@gmail.com>
2025-02-26 15:22:13 +0100

    feat: improve error messages

--- apps/web/services/api.ts
+++ apps/web/services/api.ts
@@ -3,6 +3,22 @@ import ky from "ky";
 
 export const client = createClient(process.env.NEXT_PUBLIC_ORIGIN_URL!, {
   fetch(input, requestInit, _Env, _executionCtx) {
-    return ky(input, requestInit);
+    return ky(input, {
+      ...requestInit,
+      hooks: {
+        beforeError: [
+          async (error) => {
+            const errorBody = (await error.response.json()) as {
+              message: string;
+              status: number;
+            };
+            if (errorBody?.message) {
+              throw new Error(errorBody.message);
+            }
+            return error;
+          },
+        ],
+      },
+    });
   },
 });
--- apps/web/utils/get-error-message.ts
+++ apps/web/utils/get-error-message.ts
@@ -1,8 +1,9 @@
-import { AxiosError } from "axios";
+import { HTTPError } from "ky";
 
 export const getErrorMessage = (error: unknown): string => {
-  if (error instanceof AxiosError)
-    return error.response?.data?.message || "Internal Server Error";
-  console.log("unknown error:", error);
+  if (error instanceof Error) return error.message;
+  if (error instanceof HTTPError) return error.response.statusText;
+
+  console.error("Unknown error:", error);
   return "Internal Server Error";
 };
