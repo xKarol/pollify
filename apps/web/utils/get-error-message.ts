74fa8ff549deb6b71487757e1d78af6b2514861c
Karol Piskorz <karolpiskorz8@gmail.com>
2025-02-27 21:06:21 +0100

    fix: improve error messages

--- apps/web/utils/get-error-message.ts
+++ apps/web/utils/get-error-message.ts
@@ -1,8 +1,8 @@
 import { HTTPError } from "ky";
 
 export const getErrorMessage = (error: unknown): string => {
-  if (error instanceof Error) return error.message;
   if (error instanceof HTTPError) return error.response.statusText;
+  if (error instanceof Error) return error.message;
 
   console.error("Unknown error:", error);
   return "Internal Server Error";
